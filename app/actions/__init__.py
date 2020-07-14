b7c815f3d3b26ba73d3c6a0ed52981c57804127d
medram <mramouchy@gmail.com>
2020-07-14 18:43:02 +0100

    added spam_open_messages action

---
+++ app/actions/__init__.py
@@ -0,0 +1,4 @@
+from .spam_report_all_to_inbox import Spam_report_all_to_inbox
+from .inbox_archive_all import Inbox_archive_all
+from .inbox_open_messages import Inbox_open_messages
+from .spam_open_messages import Spam_open_messages
