10370d054591a3a2c9de2550392673e583cad9d2
medram <mramouchy@gmail.com>
2020-07-14 18:40:48 +0100

    added inbox_archive_all action

---
+++ app/actions/inbox_archive_all.py
@@ -0,0 +1,57 @@
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, JavascriptException
+
+
+from app.abstract import ActionAbstract
+from app import utils, logger
+
+class Inbox_archive_all(ActionAbstract):
+
+	def apply(self):
+		logger.info(f'Processing action ({self.__class__.__name__}) for ({self.isp.profile.email})...')
+		driver = self.isp.driver
+		profile = self.isp.profile
+
+		# print('Start ActionChains...')
+		# Go to Inbox section
+		driver.get('https://mail.yahoo.com/d/folders/1')
+
+		# let javascript requests finish.
+		time.sleep(5)
+
+		# Scroll down.
+		with utils.scroll_down(driver, 'div[data-test-id=virtual-list]', ignored_exceptions=(JavascriptException,)):
+			actions = ActionChains(driver)
+			# select all msgs at spam section.
+			actions.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).perform()
+
+			time.sleep(5)
+
+			# Archive all messages.
+			try:
+				button_archive = driver.find_elements_by_css_selector('button[data-test-id=toolbar-archive]')[0]
+				button_archive.click()
+				# click to "Not junk" button.
+
+
+				time.sleep(5)
+				wait = WebDriverWait(driver, 30)
+				# undo_notification = (By.CSS_SELECTOR, 'div[role=status] div')
+				alertdialog = (By.CSS_SELECTOR, 'div[role=alertdialog] div')
+
+				if wait.until_not(EC.presence_of_element_located(alertdialog)):
+					# print('Confirm')
+					# wait to make sure the action is applied
+					time.sleep(5)
+			except TimeoutException:
+				logger.warning(f'({self.ACTION.name}) {profile.email:.<40} [WARNING]')
+			except Exception as e:
+				logger.exception(f'[{self.ACTION.name}] {profile.email:.<40} [Error]')
+			else:
+				logger.info(f'({self.ACTION.name}) {profile.email:.<40} [DONE]')
